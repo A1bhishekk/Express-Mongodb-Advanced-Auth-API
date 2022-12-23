e3b4dd76a4aae82071679ed866f3248146ec7b07
Abhishek Kumar <sahniak56@gmail.com>
2022-12-24 01:44:38 +0530

    login change password done

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -31,7 +31,7 @@ class UserControllers {
                         await newUser.save()
                         // jwt token create 
                         const saved_user= await UserModel.findOne({email:email})
-                        const token = jwt.sign({ _id: saved_user._id }, process.env.JWT_SECRET_KEY, { expiresIn: '5d' });
+                        const token = jwt.sign({ userID: saved_user._id }, process.env.JWT_SECRET_KEY, { expiresIn: '5d' });
                     //    ********************
                         return res.status(201).json({
                             status: "success",
@@ -75,13 +75,12 @@ class UserControllers {
                     const match= await bcrypt.compare(password, user.password)
                     if(match){
                         // jwt token create
-                        const token = jwt.sign({ _id: user._id }, process.env.JWT_SECRET_KEY, { expiresIn: '5d' });
+                        const token = jwt.sign({ userID: user._id }, process.env.JWT_SECRET_KEY, { expiresIn: '5d' });
 
                         return res.status(200).json({
                             status: "success",
                             message: "User logged in successfully",
                             token: token,
-                            user:user
                         })
                     }else{
                         return res.status(400).json({
@@ -116,8 +115,67 @@ class UserControllers {
         
     }
 
+    // change password api controller
 
+    static changeUserPassword= async (req, res) => {
+
+        try {
+            const { password,password_confirmation } = req.body;
+            if(password && password_confirmation){
+                if(password !== password_confirmation){
+                    res.status(400).json({
+                        status: "failed",
+                        message: "Password and confirm password does not match",
+                    })
+                }else{
+                    // hash password
+                    const salt = await bcrypt.genSalt(12);
+                    const hashedPassword = await bcrypt.hash(password, salt);
+                    // console.log(req.user)
+                    await UserModel.findByIdAndUpdate(req.user._id,{$set:{password:hashedPassword}})
+
+                    res.status(200).json({
+                        status: "success",
+                        message: "Password changed successfully",
+                        password: hashedPassword
+                    })
+                    
+                }
+
+            }else{
+                return res.status(400).json({
+                    status: "failed",
+                    message: "All fields are required",
+                })
+            }
+            
+        } catch (error) {
+            res.status(500).json({
+                status: "failed",
+                message: error.message,
+            })
+        }
+    }
+
+
+    // logged user details api controller
+
+    static loggedUser= async (req, res) => {
+        try {
+            res.status(200).json({
+                status: "success",
+                message: "User details",
+                user: req.user
+            })
+        } catch (error) {
+            res.status(500).json({
+                status: "failed",
+                message: error.message,
+            })
+            
+        }
         
+    }
 }
 
 export default UserControllers
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,11 +1,23 @@
 import express from 'express';
 import UserControllers from '../controllers/userControllers.js';
+import checkUserAuth from '../middleware/authmiddleware.js';
 const router = express.Router();
+
+// route level middleware
+
+router.use('/changepassword', checkUserAuth)
+router.use('/loggeduser', checkUserAuth)
+
+
 
 
 // public routes 
 router.post('/register', UserControllers.userRegistration);
 router.post('/login', UserControllers.userLogin);
+
+//protected routes
+router.post('/changepassword', UserControllers.changeUserPassword);
+router.get('/loggeduser', UserControllers.loggedUser);
 
 
 
