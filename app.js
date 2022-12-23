1a1cf23cc166d9424fbf6a04251c0d651f1089a3
Abhishek Kumar <sahniak56@gmail.com>
2022-12-24 05:08:57 +0530

    g

--- app.js
+++ app.js
@@ -18,7 +18,25 @@ connectDB(process.env.DATABASE_URL);
 app.use(express.json());
 
 app.get('/', (req, res) => {
-    res.send(`<h2>Technical Abhi Api is Working fine</h2>`)
+    res.send(`<!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+        <style>
+            body{
+                background-color: #000;
+                color: aqua;
+            }   
+        </style>
+    </head>
+    <body>
+        <h1>Api is working</h1>
+        
+    </body>
+    </html>`)
 })
 // load all routes
 app.use('/api/v1/user', userRoutes);
