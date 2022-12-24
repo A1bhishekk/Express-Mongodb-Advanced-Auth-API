62d7d1e4d755e4a96ac33788da14d0c68112ca09
Abhishek Kumar <sahniak56@gmail.com>
2022-12-24 15:11:14 +0530

    g

--- app.js
+++ app.js
@@ -24,25 +24,9 @@ connectDB(process.env.DATABASE_URL);
 app.use(express.json());
 
 app.get('/', (req, res) => {
-    res.send(`<!DOCTYPE html>
-    <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Document</title>
-        <style>
-            body{
-                background-color: #000;
-                color: aqua;
-            }   
-        </style>
-    </head>
-    <body>
-        <h1>Api is working</h1>
-        
-    </body>
-    </html>`)
+    res.send(`
+    <h1> Welcome to Nodejs Express Mongodb Auth Api </h1>
+     `)
 })
 // load all routes
 app.use('/api/v1/user', userRoutes);
